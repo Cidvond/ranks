4096ee45f4107823823fdc17ac6f406308877df6
Christopher Mühl <christopher@padarom.io>
2016-09-05 15:11:44 +0200

    Add branch cache

--- files/lib/system/cache/builder/RankCacheBuilder.class.php
+++ files/lib/system/cache/builder/RankCacheBuilder.class.php
@@ -1,5 +1,7 @@
 <?php namespace wcf\system\cache\builder;
 
+use wcf\data\branch\Branch;
+use wcf\data\rank\Rank;
 use wcf\system\WCF;
 
 class RankCacheBuilder extends AbstractCacheBuilder
@@ -7,17 +9,36 @@ class RankCacheBuilder extends AbstractCacheBuilder
     protected function rebuild(array $parameters)
     {
         $data = [
-            'ranks' => []
+            'ranks' => $this->getRanks(),
+            'branches' => $this->getBranches(),
         ];
 
-        // get ranks
-        $sql = "SELECT		*
-			FROM		wcf".WCF_N."_unkso_rank
-			ORDER BY	displayOrder";
+        return $data;
+    }
+
+    protected function getRanks()
+    {
+        $sql = 'SELECT * FROM ' . Rank::getDatabaseTableName() . ' ORDER BY displayOrder ASC';
         $statement = WCF::getDB()->prepareStatement($sql);
         $statement->execute();
-        while ($object = $statement->fetchObject('wcf\data\rank\Rank')) {
-            $data['ranks'][$object->categoryID][$object->rankID] = $object;
+
+        $data = [];
+        while ($object = $statement->fetchObject(Rank::class)) {
+            $data[$object->categoryID][$object->rankID] = $object;
+        }
+
+        return $data;
+    }
+
+    protected function getBranches()
+    {
+        $sql = 'SELECT * FROM ' . Branch::getDatabaseTableName() . ' ORDER BY displayOrder ASC';
+        $statement = WCF::getDB()->prepareStatement($sql);
+        $statement->execute();
+
+        $data = [];
+        while ($object = $statement->fetchObject(Branch::class)) {
+            $data[$object->branchID] = $object;
         }
 
         return $data;
